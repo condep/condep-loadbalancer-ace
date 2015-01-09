d421e9948b29d04dc52f001e852a1e277be26497
Jon Arild Torresdal <jon@torresdal.net>
2015-01-09 12:49:38 +0100

    Added missing build stuff

--- src/ConDep.Dsl.LoadBalancer.Ace/Properties/AssemblyInfo.cs
+++ src/ConDep.Dsl.LoadBalancer.Ace/Properties/AssemblyInfo.cs
@@ -1,11 +1,18 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ConDep.Dsl.LoadBalancer.Ace")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ConDep integration with the Cisco ACE Load Balancer.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("ConDep")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © ConDep 2014")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3478cec1-aa5d-4b3e-aa3c-2d0b57e94c81")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: CLSCompliant(true)]
